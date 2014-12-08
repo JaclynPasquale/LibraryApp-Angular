4f6e2d3ffec409cfb062d989aadd172b3921c620
jaclynpasquale <jaclynpasquale@gmail.com>
2014-12-08 16:19:41 -0600

    using routes and ng-includes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,52 @@
 ;(function(){
   'use strict';
 
-  angular.module('LibraryApp', [])
-    .controller('LibraryAppController', function($http){
+  angular.module('LibraryApp', ['ngRoute'])
+
+    .config(function($routeProvider){
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/table.html',
+        controller: 'LibraryAppController',
+        controllerAs: 'laCtrl'
+      })
+      .when('/new', {
+        templateUrl: 'views/form.html',
+        controller: 'LibraryAppController',
+        controllerAs: 'laCtrl'
+      })
+      .when('/:id', {
+        templateUrl: 'views/show.html',
+        controller: 'ShowController',
+        controllerAs: 'show'
+
+      })
+      .otherwise({redirectTo: '/'})
+    })
+
+    .controller('ShowController', function($http, $routeParams){
       var vm = this;
+      var id = $routeParams.id;
+      $http.get('https://library-app-angular.firebaseio.com/' + id + '.json')
+        .success(function(data){
+          vm.book = data;
+        })
+        .error(function(err){
+          console.log(err);
+        })
+    })
+
+
+    .controller('LibraryAppController', function($http, $routeParams){
+      var vm = this;
+      console.log($routeParams);
       // i don't get why we're doing this here
 
 
       $http.get('https://library-app-angular.firebaseio.com/.json')
       .success(function(data){
         vm.books = data;
+
 
       })
       .error(function(err){
@@ -21,8 +58,10 @@
   vm.addNewBook = function(){
     $http.post('https://library-app-angular.firebaseio.com/.json', vm.newBook)
     .success(function(data){
-      vm.books.push(vm.newBook);
-      vm.newBook = freshBook();
+      vm.books[data.name] = vm.newBook;
+      vm.newBook = "";
+      //vm.books.push(vm.newBook);
+      //vm.newBook = freshBook();
     })
 
   .error(function(err){
@@ -34,23 +73,15 @@
   vm.delete = function(bookId){
     $http.delete('https://library-app-angular.firebaseio.com/' + bookId + '.json' )
     .success(function(){
-      var index = vm.books.indexOf(bookId);
-      vm.books.splice(index,1);
+      delete vm.books[bookId];
+      //var index = vm.books.indexOf(bookId);
+      //vm.books.splice(index,1);
     })
     .error(function(err){
       console.log(err);
     })
   };
 
-
-
-  vm.newBook = freshBook();
-
-  function freshBook(){
-    return{
-
-  }
-}
 
     });
 
