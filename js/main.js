b0c1609dce4021f791d2aa3919f4855790a9e485
jaclynpasquale <jaclynpasquale@gmail.com>
2014-12-07 23:28:42 -0600

    inital commit setting up files

---
+++ js/main.js
@@ -0,0 +1,43 @@
+;(function(){
+  'use strict';
+
+  angular.module('LibraryApp', []){
+    .controller('LibraryAppController', function(){
+      var vm = this;
+      // i don't get why we're doing this here
+
+      vm.books = [
+      {
+        title: 'I Could Pee on This: And Other Poems by Cats',
+        author: 'Francesco Marciuliano',
+        price: '8.50',
+        image: 'http://ecx.images-amazon.com/images/I/51axz2dw8pL._SY344_BO1,204,203,200_.jpg'
+      },
+      {
+        title: 'How to Tell If Your Cat Is Plotting to Kill You',
+        author: 'The Oatmeal and Matthew Inman',
+        price: '8.99',
+        image: 'http://ecx.images-amazon.com/images/I/41%2B57E7XY4L._SL300.jpg'
+      },
+      {
+        title: 'Sorry I Barfed on Your Bed (and Other Heartwarming Letters from Kitty)',
+        author: 'Allia Zobel Nolan',
+        price: '9.99',
+        image: 'http://ecx.images-amazon.com/images/I/51OdPpFcX4L._SX258_BO1,204,203,200_.jpg'
+      },
+      {
+        title: 'Grumpy Cat: A Grumpy Book',
+        author: 'Grumpy Cat',
+        price: '9.30',
+        image: 'http://ecx.images-amazon.com/images/I/51SszqxmPyL._SY344_BO1,204,203,200_.jpg'
+      },
+        ]
+    });
+
+
+});
+
+
+
+})();
+//
